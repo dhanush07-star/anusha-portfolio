8d9be5326be743de16dece7ef216fad69b46dd2b
Dhanush <gdhanush@am.students.amrita.edu>
2023-08-28 19:47:18 +0530

    made few changed in icons

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookF, faGoogle } from "@fortawesome/free-brands-svg-icons";
-import { faDesktop, faFileCode } from "@fortawesome/free-solid-svg-icons";
+import { faDesktop, faFileCode,faBook,faBlog } from "@fortawesome/free-solid-svg-icons";
+//<FontAwesomeIcon icon={} />
+//<FontAwesomeIcon icon="fa-solid fa-blog" />
 
 const Services = () => {
   return (
@@ -28,7 +30,10 @@ const Services = () => {
           {/* - */}
           <div className="col-lg-3 col-md-6 col-sm-6">
             <div className="box">
-              <div className="circle"><FontAwesomeIcon className="icon" icon={faFacebookF} size="2x" /></div>
+              <div className="circle">
+                <FontAwesomeIcon className="icon" icon={faBlog} size="2x" />
+                {/* <FontAwesomeIcon className="icon" size="2x" icon="fa-solid fa-blog"  /> */}
+                </div>
 
               <h3>Blogs</h3>
               <p>Your potential clients well see your services or product on Facebook</p>
@@ -37,7 +42,7 @@ const Services = () => {
           {/* - */}
           <div className="col-lg-3 col-md-6 col-sm-6">
             <div className="box">
-              <div className="circle"><FontAwesomeIcon className="icon" icon={faGoogle} size="2x" /></div>
+              <div className="circle"><FontAwesomeIcon className="icon" icon={faBook} size="2x" /></div>
 
               <h3>Thesis</h3>
               <p>Your service or product will appear at the top of the Google search</p>.
